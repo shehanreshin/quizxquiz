48e7cb2fc5d5eb6b8cc777d579c74ae03516c1c9
shehanreshin <asp.shehanreshin@gmail.com>
2023-12-29 22:47:30 +0530

    Initialize project

---
+++ quizxquiz/src/main/java/edu/personal/quizxquiz/QuizxquizApplication.java
@@ -0,0 +1,13 @@
+package edu.personal.quizxquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizxquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizxquizApplication.class, args);
+	}
+
+}
---
+++ quizxquiz/src/test/java/edu/personal/quizxquiz/QuizxquizApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.personal.quizxquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizxquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
