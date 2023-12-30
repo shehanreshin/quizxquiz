8e0731aa16390cda91ddf2c2e1bccae4f6f9bbdd
shehanreshin <asp.shehanreshin@gmail.com>
2023-12-30 14:17:27 +0530

    Initialize project

--- src/main/java/edu/personal/quizxquiz/AppInitializer.java
+++ src/main/java/edu/personal/quizxquiz/AppInitializer.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuizxquizApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(QuizxquizApplication.class, args);
-	}
-
+public class AppInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(AppInitializer.class, args);
+    }
 }
