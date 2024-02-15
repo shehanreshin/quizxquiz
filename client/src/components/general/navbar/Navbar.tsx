8022f5e3a3e4336a57890eaac83183d4c873a8d7
shehanreshin <asp.shehanreshin@gmail.com>
2024-02-15 15:32:09 +0530

    Update navbar by deleting unused icons

--- client/src/components/general/navbar/Navbar.tsx
+++ client/src/components/general/navbar/Navbar.tsx
@@ -12,8 +12,7 @@ function Navbar() {
                     <ul className="nav nav-pills flex-column mt-5 mb-auto fw-medium">
                         <li className="nav-item mb-4">
                             <a href="#" className="nav-link" aria-current="page">
-                                <svg className="bi pe-none me-2" width="16" height="16"></svg>
-                                Home
+                                <span className='ps-4'>Home</span>
                             </a>
                         </li>
                         <li>
@@ -23,38 +22,32 @@ function Navbar() {
                         </li>
                         <li>
                             <a href="#" className="nav-link link-body-emphasis">
-                                <svg className="bi pe-none me-2" width="16" height="16"></svg>
-                                <span>My presentations</span>
+                                <span className='ps-4'>My presentations</span>
                             </a>
                         </li>
                         <li>
                             <a href="#" className="nav-link link-body-emphasis">
-                                <svg className="bi pe-none me-2" width="16" height="16"></svg>
-                                <span>My quizzes</span>
+                                <span className='ps-4'>My quizzes</span>
                             </a>
                         </li>
                         <li>
                             <a href="#" className="nav-link link-body-emphasis mb-5">
-                                <svg className="bi pe-none me-2" width="16" height="16"></svg>
-                                <span>Shared with me</span>
+                                <span className='ps-4'>Shared with me</span>
                             </a>
                         </li>
                         <li>
                             <a href="#" className="nav-link link-body-emphasis">
-                                <svg className="bi pe-none me-2" width="16" height="16"></svg>
-                                <span>Templates</span>
+                                <span className='ps-4'>Templates</span>
                             </a>
                         </li>
                         <li>
                             <a href="#" className="nav-link link-body-emphasis">
-                                <svg className="bi pe-none me-2" width="16" height="16"></svg>
-                                <span>Help & support</span>
+                                <span className='ps-4'>Help & support</span>
                             </a>
                         </li>
                         <li>
                             <a href="#" className="nav-link link-body-emphasis">
-                                <svg className="bi pe-none me-2" width="16" height="16"></svg>
-                                <span>Trash</span>
+                                <span className='ps-4'>Trash</span>
                             </a>
                         </li>
                     </ul>
